c7c2b5ef8622114434dd061e124b7370b21becb3
RAMESH <dhageramesh143@gmail.com>
2023-03-16 23:21:11 +0530

    hashmap

---
+++ MainProject/src/com/demo/Map.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import java.util.HashMap;
+
+public class Map {
+	public static void main(String[] args) {
+		
+		HashMap<String, String> map=new HashMap<String,String>();
+		
+		map.put("ramesh", "dhage");
+		map.put("sudhakar", "dhonde");
+		map.put("shivaji", "shinde");
+		map.put("satish", "mule");
+		
+		System.out.println(map);
+	}
+
+}
