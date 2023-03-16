1795221cdf2e437d346276dee3e87e59797c048e
RAMESH <dhageramesh143@gmail.com>
2023-03-16 21:33:40 +0530

    added arraylist

---
+++ MainProject/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.demo;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String > al=new ArrayList<String>();
+		al.add("pawan");
+		al.add("shiva ");
+		al.add("shashi");
+		al.add("rawi");
+		
+		System.out.println(al);
+	}
+
+}
